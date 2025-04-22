9e12555fe3822c924c1e6cd57344f02151af249f
程序员小墨 <codingzhang@126.com>
2025-04-23 05:07:50 +0800

    fix: 修复 gradle build 报错

--- frost-iot-adapter/frost-iot-adapter-mqtt/src/main/java/com/footprintcat/frostiot/adapter/mqtt/spi/IMQTTAdapter.java
+++ frost-iot-adapter/frost-iot-adapter-mqtt/src/main/java/com/footprintcat/frostiot/adapter/mqtt/spi/IMQTTAdapter.java
@@ -1,5 +1,7 @@
 package com.footprintcat.frostiot.adapter.mqtt.spi;
 
+import com.footprintcat.frostiot.adapter.spi.IAdapter;
+
 public interface IMQTTAdapter extends IAdapter {
 
     void connect();
--- manager/backend/src/test/java/com/footprintcat/frostiot/manager/ManagerApplicationTests.java
+++ manager/backend/src/test/java/com/footprintcat/frostiot/manager/ManagerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.footprintcat.frostiot.manager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
