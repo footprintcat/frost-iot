0d696f854f2b07bf4f25c3e6a1825a79ec6d604c
程序员小墨 <codingzhang@126.com>
2025-04-25 05:11:02 +0800

    feat(utils): StringUtils 新增 leftPad, rightPad 方法

--- common/src/main/java/com/footprintcat/frostiot/common/utils/StringUtils.java
+++ common/src/main/java/com/footprintcat/frostiot/common/utils/StringUtils.java
@@ -57,4 +57,68 @@ public class StringUtils {
     public static String trimToEmpty(String str, String defaultStr) {
         return str == null ? defaultStr : str.trim();
     }
+
+    /**
+     * 在字符串左侧填充指定字符，直到达到指定长度
+     *
+     * @param str     原始字符串，如果为null则视为空字符串
+     * @param size    填充后的总长度
+     * @param padChar 用于填充的字符
+     * @return 填充后的字符串。如果原始字符串长度已经等于或大于size，则返回原始字符串
+     * @since 2025-04-25
+     */
+    public static String leftPad(String str, int size, char padChar) {
+        if (str == null) {
+            str = "";
+        }
+
+        int strLength = str.length();
+        if (strLength >= size) {
+            return str;
+        }
+
+        // StringBuilder sb = new StringBuilder(size);
+        // for (int i = 0; i < size - strLength; i++) {
+        //     sb.append(padChar);
+        // }
+        // sb.append(str);
+        // return sb.toString();
+        return String.valueOf(padChar).repeat(size - strLength) + str;
+    }
+
+    public static String leftPad(String str, int size) {
+        return leftPad(str, size, ' ');
+    }
+
+    /**
+     * 在字符串右侧填充指定字符，直到达到指定长度
+     *
+     * @param str     原始字符串，如果为null则视为空字符串
+     * @param size    填充后的总长度
+     * @param padChar 用于填充的字符
+     * @return 填充后的字符串。如果原始字符串长度已经等于或大于size，则返回原始字符串
+     * @since 2025-04-25
+     */
+    public static String rightPad(String str, int size, char padChar) {
+        if (str == null) {
+            str = "";
+        }
+
+        int strLength = str.length();
+        if (strLength >= size) {
+            return str;
+        }
+
+        // StringBuilder sb = new StringBuilder(size);
+        // sb.append(str);
+        // for (int i = strLength; i < size; i++) {
+        //     sb.append(padChar);
+        // }
+        // return sb.toString();
+        return str + String.valueOf(padChar).repeat(size - strLength);
+    }
+
+    public static String rightPad(String str, int size) {
+        return rightPad(str, size, ' ');
+    }
 }
