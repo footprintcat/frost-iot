8e40da1953a9912721d09040e58c690bedce7737
程序员小墨 <codingzhang@126.com>
2025-04-25 02:29:54 +0800

    feat(core): 使用 @Slf4j 打印日志

--- frost-iot-core/src/main/java/com/footprintcat/frostiot/core/api/HelloController.java
+++ frost-iot-core/src/main/java/com/footprintcat/frostiot/core/api/HelloController.java
@@ -7,7 +7,9 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
 import io.micronaut.http.annotation.QueryValue;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 public class HelloController {
 
@@ -19,7 +21,8 @@ public class HelloController {
 
     @Get("/hi")
     public String hi() {
-        return "Hi";
+        log.info("[GET] hi");
+        return "你好"; // 测试中文是否乱码
     }
 
     @Get("/isEmpty")
--- frost-iot-core/src/main/java/com/footprintcat/frostiot/core/internal/LifetimeEventListener.java
+++ frost-iot-core/src/main/java/com/footprintcat/frostiot/core/internal/LifetimeEventListener.java
@@ -4,6 +4,7 @@ import io.micronaut.runtime.event.annotation.EventListener;
 import io.micronaut.runtime.server.event.ServerShutdownEvent;
 import io.micronaut.runtime.server.event.ServerStartupEvent;
 import jakarta.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * core 项目生命周期事件
@@ -11,17 +12,18 @@ import jakarta.inject.Singleton;
  * @since 2025-04-25
  */
 @Singleton
+@Slf4j
 public class LifetimeEventListener {
 
     @EventListener
     public void onStartup(ServerStartupEvent event) {
-        System.out.println("Micronaut 应用已启动！");
+        log.info("Micronaut 应用已启动！");
         // 在这里写初始化逻辑
     }
 
     @EventListener
     public void onShutdown(ServerShutdownEvent event) {
-        System.out.println("应用正在停止...");
+        log.info("应用正在停止...");
         // 执行清理逻辑（如关闭数据库连接、释放资源）
     }
 }
