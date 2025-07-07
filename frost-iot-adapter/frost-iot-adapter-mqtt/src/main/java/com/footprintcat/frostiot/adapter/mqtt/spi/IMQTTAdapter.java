1f5abab5e29d6e3f40ed3c83fc81c60fc77a542f
小墨 <codingzhang@126.com>
2025-07-07 16:55:24 +0800

    feat(adapter): adapter 实现类迁移至 frost-iot-adapter 代码仓库

--- frost-iot-adapter/frost-iot-adapter-mqtt/src/main/java/com/footprintcat/frostiot/adapter/mqtt/spi/IMQTTAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.footprintcat.frostiot.adapter.mqtt.spi;
-
-import com.footprintcat.frostiot.adapter.sdk.spi.IAdapter;
-
-public interface IMQTTAdapter extends IAdapter {
-
-    void connect();
-}
