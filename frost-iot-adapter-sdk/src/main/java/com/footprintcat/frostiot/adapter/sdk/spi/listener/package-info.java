d6b0fab517ee5d7b02f953b4950492ab42a6f13a
小墨 <codingzhang@126.com>
2025-09-27 23:07:47 +0800

    copyright(project): 项目代码添加版权头

--- frost-iot-adapter-sdk/src/main/java/com/footprintcat/frostiot/adapter/sdk/spi/listener/package-info.java
+++ frost-iot-adapter-sdk/src/main/java/com/footprintcat/frostiot/adapter/sdk/spi/listener/package-info.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 武汉脚印猫科技有限公司 (Wuhan Footprint Cat Technology Co., Ltd.)
+ *
+ * This source code is licensed under the BSD-3-Clause license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 /**
  * 设备侧主动上报数据，Adapter 侧被动接收消息
  *
