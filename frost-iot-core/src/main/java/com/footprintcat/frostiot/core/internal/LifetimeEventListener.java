fb38259760be58aa798bb8ea56de30cd9651c25a
小墨 <codingzhang@126.com>
2025-10-15 10:11:11 +0800

    feat(core): 启动后立即打印版权信息，不再隐藏 RuntimeConfiguredEnvironment 日志

--- frost-iot-core/src/main/java/com/footprintcat/frostiot/core/internal/LifetimeEventListener.java
+++ frost-iot-core/src/main/java/com/footprintcat/frostiot/core/internal/LifetimeEventListener.java
@@ -49,9 +49,6 @@ public class LifetimeEventListener {
         // 解决 log.info 在 Windows 系统自带命令行打印时中文乱码问题
         charset = EncodingInitializer.init();
         isUtf8Charset = StandardCharsets.UTF_8.equals(charset);
-
-        // 打印版权信息
-        SystemInfoUtils.printCopyright(moduleInfo);
     }
 
     @EventListener
