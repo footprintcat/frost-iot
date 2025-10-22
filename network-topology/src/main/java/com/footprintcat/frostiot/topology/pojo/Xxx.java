138343a354c029f73ef3869dc14a2de215df33c2
小墨 <codingzhang@126.com>
2025-10-22 15:49:45 +0800

    chore(topology): 创建网络拓扑模块目录结构

---
+++ network-topology/src/main/java/com/footprintcat/frostiot/topology/pojo/Xxx.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 武汉脚印猫科技有限公司 (Wuhan Footprint Cat Technology Co., Ltd.)
+ *
+ * This source code is licensed under the BSD-3-Clause license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package com.footprintcat.frostiot.topology.pojo;
+
+/**
+ * @since 2025-10-22
+ */
+public class Xxx {
+}
