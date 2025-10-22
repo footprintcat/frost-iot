1795593ae625252ba71c88b5db73ca98be37e687
郭枭 <guoxiao@footprintcat.com>
2025-10-22 17:39:02 +0800

    创建通信工具接口及实现类模板

---
+++ network-topology/src/main/java/com/footprintcat/frostiot/topology/communicate/webSocket/WebSocketCommunicationTool.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 武汉脚印猫科技有限公司 (Wuhan Footprint Cat Technology Co., Ltd.)
+ *
+ * This source code is licensed under the BSD-3-Clause license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package com.footprintcat.frostiot.topology.communicate.webSocket;
+
+import com.footprintcat.frostiot.topology.communicate.CommunicationConfig;
+import com.footprintcat.frostiot.topology.communicate.CommunicationTool;
+import com.footprintcat.frostiot.topology.communicate.CommunicationType;
+
+public class WebSocketCommunicationTool implements CommunicationTool {
+    @Override
+    public void init(CommunicationConfig config) {
+        System.out.println("WebSocket初始化...");
+    }
+
+    @Override
+    public void sendMessage(String message, String target) {
+        System.out.println("WebSocket发消息.");
+    }
+
+    @Override
+    public void shutdown() {
+        System.out.println("WebSocket关闭连接.");
+    }
+
+    @Override
+    public CommunicationType getType() {
+        return CommunicationType.WEBSOCKET;
+    }
+
+    @Override
+    public boolean isConnected() {
+        return false;
+    }
+}
