eec9829dbdb4cdd0a3d50bfd0f4e3a859858d56a
小墨 <codingzhang@126.com>
2025-10-23 10:40:51 +0800

    chore(license): 更新文件头缺失版权

--- frost-iot-core-springboot/src/main/java/com/footprintcat/frostiot/core/springboot/config/MybatisPlusConfig.java
+++ frost-iot-core-springboot/src/main/java/com/footprintcat/frostiot/core/springboot/config/MybatisPlusConfig.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 武汉脚印猫科技有限公司 (Wuhan Footprint Cat Technology Co., Ltd.)
+ *
+ * This source code is licensed under the BSD-3-Clause license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 package com.footprintcat.frostiot.core.springboot.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
