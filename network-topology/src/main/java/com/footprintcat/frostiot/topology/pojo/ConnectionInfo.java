2c1ab113a182df25354405f11577f6e0bef01fa8
郭枭 <guoxiao@footprintcat.com>
2025-10-30 16:22:59 +0800

    feat(topology): 新增连接信息类

---
+++ network-topology/src/main/java/com/footprintcat/frostiot/topology/pojo/ConnectionInfo.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 武汉脚印猫科技有限公司 (Wuhan Footprint Cat Technology Co., Ltd.)
+ *
+ * This source code is licensed under the BSD-3-Clause license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package com.footprintcat.frostiot.topology.pojo;
+
+import lombok.Data;
+
+@Data
+public class ConnectionInfo {
+
+    private String targetNodeId;
+    private String targetNodeType;
+    private String relation;
+
+    //...可扩展连接质量
+}
